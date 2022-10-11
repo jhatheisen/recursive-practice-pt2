d414a100b49581ea7eb8d8a949de0d5e5b4874a9
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-11 13:15:29 -0700

    finished problem 5 and 6

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -10,12 +10,35 @@ reverse("atom"); // "mota"
 reverse("q"); // "q"
 reverse("id"); // "di"
 reverse(""); // ""
+
+1. saved the first letter
+2. saved the rest of the characters as var
+3. called the reverse with the str
+4. added first letter after the string
+5. if str.length === 0, returns ''
+
 ***********************************************************************/
 
 
 function reverse(str) {
   // Your code here
+  let firstLetter = str[0];
+  let rest = str.slice(1);
+
+  if (str.length === 0) {
+    return '';
+  }
+
+  return reverse(rest) + firstLetter;
+
 }
+
+reverse("house"); // "esuoh"
+reverse("dog"); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -1,7 +1,5 @@
 /***********************************************************************
-Write a recursive function called `addToTwelve` that will return true if there
-are two adjacent numbers in the input array that can sum up to 12. Otherwise,
-return false.
+
 
 Examples:
 
@@ -10,13 +8,43 @@ addToTwelve([1, 3, 4, 7, 6]); // false
 addToTwelve([1, 11, 4, 7, 6]); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
+
+function sumToN(n) {
+  // Your code here
+  // if starts at or gets to 0, null + 1 = 1
+  if (n <= 0 ) {
+    return null;
+  }
+
+  // sets result to n + n-1,
+  let result = sumToN(n - 1) + n;
+  // after every num added return result
+  return result === 12;
+}
+
+Write a recursive function called `addToTwelve` that will return true if there
+are two adjacent numbers in the input array that can sum up to 12. Otherwise,
+return false.
+
 ***********************************************************************/
 
 
 function addToTwelve(arr) {
   // Your code here
+  if (arr[0] + arr[1] === 12) {
+    return true;
+  } else if (arr.length <= 1) {
+    return false;
+  }
+  arr.shift();
+  return addToTwelve(arr);
 }
 
+addToTwelve([1, 3, 4, 7, 5]); // true
+// addToTwelve([1, 3, 4, 7, 6]); // false
+// addToTwelve([1, 11, 4, 7, 6]); // true
+// addToTwelve([1, 12, 4, 7, 6]); // false
+// addToTwelve([1]); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
